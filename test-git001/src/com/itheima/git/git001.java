692478a4c930dc988bb01a5a104957c03018f173
zhangsan <zhangsan@itcast.cn>
2019-05-04 19:44:54 +0800

    修改了git001

--- test-git001/src/com/itheima/git/git001.java
+++ test-git001/src/com/itheima/git/git001.java
@@ -4,10 +4,12 @@ public class git001 {
 
     public static void main(String[] args) {
         System.out.println("---git001---");
+        //hello test
     }
 
     public void test(){
         int i = 0 ;
         i += 10;
+        System.out.println(i);
     }
 }
