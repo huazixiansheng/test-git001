8462fec1743d81092bf7b0b5183df1f0d949f3c8
zhangsan <zhangsan@itcast.cn>
2019-05-04 19:29:41 +0800

    第一次提交git到版本库-修改

--- test-git001/src/com/itheima/git/git001.java
+++ test-git001/src/com/itheima/git/git001.java
@@ -5,4 +5,9 @@ public class git001 {
     public static void main(String[] args) {
         System.out.println("---git001---");
     }
+
+    public void test(){
+        int i = 0 ;
+        i += 10;
+    }
 }
